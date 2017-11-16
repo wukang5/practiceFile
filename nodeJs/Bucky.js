44cc9d66b564fb752da3168dd2bb9349ef0ff9e4
wukang5 <m15027723094_1@163.com>
2017-11-17 07:17:46 +0800

    2017-11-17
    +模块共享

---
+++ nodeJs/Bucky.js
@@ -0,0 +1,4 @@
+//引入movie.js模块
+var Bucky = require("./movie");
+Bucky.favMovie = "羞羞的铁拳";
+console.log(Bucky.favMovie);
---
+++ nodeJs/Emlly.js
@@ -0,0 +1,3 @@
+//引入movie.js模块
+var Emlly = require("./movie");
+console.log(Emlly.favMovie);
